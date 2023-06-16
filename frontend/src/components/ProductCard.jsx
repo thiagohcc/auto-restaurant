5014c878bf480b5b5f91fae79dda0bfec0dd390a
thiagohcc <dzno23@gmail.com>
2023-06-15 23:07:12 -0300

    FEAT: imposes a layout for viewing the product card on the screen

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import './styles/productCard.css'
 
 export default function ProductCard({product}) {
   return (
-    <div>
-      <img src={ product.img } alt={ product.name }/>
-      <h2>{ product.name }</h2>
-      <h3>{ product.price }</h3>
-      <p>{ product.detalhes }</p>
-      <label>
-        Qtd.:
-        <input type="number" />
-      </label>
-      <button type="button">
-        PEDIR
-      </button>
+    <div id='product_card'>
+      <img class='product_image' src={ product.img } alt={ product.name }/>
+      <div class='product_data'>
+        <h2>{ product.name }</h2>
+        <h3>{ product.price }</h3>
+        <p>{ product.detalhes }</p>
+        <label>
+          Qtd.:
+          <input type="number" />
+        </label>
+        <button type="button">
+          PEDIR
+        </button>
+      </div>
     </div>
   )
 };
