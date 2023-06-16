a7b760bf0c8ede6f9b8184901c789bbaf9e58ba6
thiagohcc <dzno23@gmail.com>
2023-06-15 23:11:29 -0300

    FIX: fix class assignment in react pattern as className

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -4,8 +4,8 @@ import './styles/productCard.css'
 export default function ProductCard({product}) {
   return (
     <div id='product_card'>
-      <img class='product_image' src={ product.img } alt={ product.name }/>
-      <div class='product_data'>
+      <img className='product_image' cl src={ product.img } alt={ product.name }/>
+      <div className='product_data'>
         <h2>{ product.name }</h2>
         <h3>{ product.price }</h3>
         <p>{ product.detalhes }</p>
